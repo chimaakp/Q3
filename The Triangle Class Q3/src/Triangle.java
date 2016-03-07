5a70724b140389fa361462646593abab8e55e1dc
chimaakp <chimaakp@ChimaII>
2016-03-07 00:00:09 -0500

    making progress

--- The Triangle Class Q3/src/Triangle.java
+++ The Triangle Class Q3/src/Triangle.java
@@ -1,14 +1,30 @@
 
 public class Triangle {
-	int side1 = 1;
-	int side2 = 1;
-	int side3 = 1;
+	double side1 = 1;
+	double side2 = 1;
+	double side3 = 1;
 	
 	public static void main(String[] args) {
 		
+	
+	Triangle triangle1 = new Triangle(4, 3, 2);
 	}
-	Triangle triangle = new Triangle(side1, side2, side3)
+	
+	Triangle() {
+	}
+	
+	Triangle(double side1, double side2,double side3) {
+		side1 = this.side1;
+		side2 = this.side2;
+		side3 = this.side3;		
+	}
+			
+	//gets the perimeter		
+	double getPerimeter() {
+		return (side1 + side2 + side3)/2;	
+	}
+	
 	double getArea() {
-		return
+		return Math.sqrt(side1)
 	}
 }
