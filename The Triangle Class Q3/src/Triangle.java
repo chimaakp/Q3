72ea24f2b531247e149532a20f3be4444dea8313
chimaakp <chimaakp@ChimaII>
2016-03-06 23:27:48 -0500

    starting

---
+++ The Triangle Class Q3/src/Triangle.java
@@ -0,0 +1,14 @@
+
+public class Triangle {
+	int side1 = 1;
+	int side2 = 1;
+	int side3 = 1;
+	
+	public static void main(String[] args) {
+		
+	}
+	Triangle triangle = new Triangle(side1, side2, side3)
+	double getArea() {
+		return
+	}
+}
