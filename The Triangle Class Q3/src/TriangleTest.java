21e396c189f33b5cc431d9b91f70b0eb66177150
chimaakp <chimaakp@ChimaII>
2016-03-07 16:55:23 -0500

    idek

---
+++ The Triangle Class Q3/src/TriangleTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TriangleTest {
+
+	@Test
+	public void testTriangle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTriangleDoubleDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGethP() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetArea() {
+		fail("Not yet implemented");
+	}
+
+}
