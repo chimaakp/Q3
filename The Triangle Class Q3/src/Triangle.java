7335a405cfb468751e332ba38dfeb2d1635fe49c
chimaakp <chimaakp@ChimaII>
2016-03-07 00:42:52 -0500

    runs but needs junit tests

--- The Triangle Class Q3/src/Triangle.java
+++ The Triangle Class Q3/src/Triangle.java
@@ -8,7 +8,9 @@ public class Triangle {
 		
 	
 	Triangle triangle1 = new Triangle(4, 3, 2);
+	System.out.printf("The area of a circle with sides of length %d, %d and %d is %f", 4, 3, 2, triangle1.getArea());
 	}
+	
 	
 	Triangle() {
 	}
@@ -20,11 +22,12 @@ public class Triangle {
 	}
 			
 	//gets the perimeter		
-	double getPerimeter() {
+	double gethP() {
 		return (side1 + side2 + side3)/2;	
 	}
 	
 	double getArea() {
-		return Math.sqrt(side1)
+		return Math.sqrt(gethP() * (gethP() - side1) *
+		(gethP() - side2) * (gethP() - side3));
 	}
 }
