cad5be5009a366e8a7c20ab37a80c871471b05bd
chimaakp <chimaakp@ChimaII>
2016-03-08 03:01:12 -0500

    fin

--- The Triangle Class Q3/src/TriangleTest.java
+++ The Triangle Class Q3/src/TriangleTest.java
@@ -16,7 +16,7 @@ public class TriangleTest {
 	@Test
 	public void testGetArea() {
 		Triangle t2 = new Triangle(4, 3, 5);
-		double Ar = 6
+		double Ar = 6;
 		
 		assertEquals( t2.getArea(), Ar);
 		
